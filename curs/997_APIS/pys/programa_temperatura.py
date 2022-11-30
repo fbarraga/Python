c794254f9622dbd2480eadb40dfc642a714e295c
Francesc Barragán Pascual <fbarraga@xtec.cat>
2022-11-30 12:06:35 +0100

    Update

--- curs/997_APIS/pys/programa_temperatura.py
+++ curs/997_APIS/pys/programa_temperatura.py
@@ -1,6 +1,14 @@
 import requests
 import os
+
+
+
 api_key=os.getenv("open_wheather_key")
+# Associada a Francescbarragan@msn.com
+# Es pot solicitar una apikey directament a traves del registre de openweathermap
+# Despues de darse de alta no es usable hasta al cabo de unas horas
+
+api_key= "3c36336006bb9fa6193c4359165315a3"
 ciudad=input("Dime el nombre de una ciudad:")
 
 parametros={"q":ciudad,"mode":"json","units":"metric","APPID":api_key}
