c50b663efc384300490e7cd263ee32eb1fb2d497
Francesc Barragan <francescbarragan@msn.com>
2023-02-20 14:55:17 +0100

    Update

--- curs/007_FILES/u36d/pys/escriure-dades-llista.py
+++
@@ -1,5 +0,0 @@
-import pickle
-llista = ['anna','lluis','pere','laura','maria','joan','manel','joan']
-fitxer = open('dades.pckl','wb')
-pickle.dump(llista,fitxer)
-fitxer.close
--- curs/007_FILES/u36d/pys/llegir_dades_llista.py
+++
@@ -1,5 +0,0 @@
-import pickle
-fitxer = open('dades.pckl','rb')
-llista = pickle.load(fitxer)
-print(llista)
-fitxer.close
