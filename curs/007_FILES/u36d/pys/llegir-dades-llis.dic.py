c46de949a31022ae8b70a43cd3187dd8ba1321b3
Francesc Barragán Pascual <fbarraga@xtec.cat>
2023-03-24 08:33:52 +0100

    Update

--- curs/007_FILES/u36d/pys/llegir-dades-llis.dic.py
+++ curs/007_FILES/u36d/pys/llegir-dades-llis.dic.py
@@ -1,8 +1,10 @@
 import pickle
+
 fitxer = open('dades.pckl','rb')
 ll = pickle.load(fitxer)
 for e in ll:
-nom = e['nom']
-cognom = e['cognom']
-print(f"{cognom}, {nom}")
+    nom = e['nom']
+    cognom = e['cognom']
+    print(f"{cognom}, {nom}")
+
 fitxer.close
