24911d9339a2751e977ddcb3ac5fb898bcbca0d8
Francesc Barragan <fbarraga@xtec.cat>
2023-04-21 17:52:49 +0200

    Update

--- curs/016_APIS/pys/weather/programa_temperatura.py
+++ curs/016_APIS/pys/weather/programa_temperatura.py
@@ -6,13 +6,15 @@ api_key=os.getenv("open_wheather_key")
 # Es pot solicitar una apikey directament a traves del registre de openweathermap
 # Despues de darse de alta no es usable hasta al cabo de unas horas
 
-api_key= "3c36336006bb9fa6193c4359165315a3"
+api_key ="b106289e4b4fcfdaa3d219da7118a2e1" #fbarraga@institutcampalans.net
+
 ciudad=input("Dime el nombre de una ciudad:")
 
 parametros={"q":ciudad,"mode":"json","units":"metric","APPID":api_key}
 r=requests.get("http://api.openweathermap.org/data/2.5/weather",    params=parametros)
 if r.status_code == 200:
     datos = r.json()
+    print(datos)
     print("La temperatura actual es:",datos["main"]["temp"])
 else:
     print("De esa ciudad no tengo datos.")
